8b4060d65c137414860cfc8845d5d9d514c04273
khm1995 <khaled.haj.moustafa.khm.1995@gmail.com>
2022-09-18 21:50:38 +0300

    generate client in domain layer

---
+++ lib/src/users_managments/domain/client.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'client.freezed.dart';
+part 'client.g.dart';
+
+enum PreferredServiceType { online, home, office }
+
+@freezed
+class Client with _$Client {
+  factory Client({
+    @JsonKey(name: '_id') String? id,
+    String? uID,
+    required PreferredServiceType preferredServiceType,
+    required List<String> diseases,
+    required Map<String, Object> preferences,
+  }) = _Client;
+  factory Client.fromJson(Map<String, dynamic> json) => _$ClientFromJson(json);
+}

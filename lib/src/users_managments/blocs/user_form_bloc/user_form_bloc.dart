296c79a75590315515f873aae7c77f4df9c371fc
bassamalraheam <88142583+bassamalraheam@users.noreply.github.com>
2022-09-19 21:52:15 +0300

    user_form_bloc

--- lib/src/users_managments/blocs/user_form_bloc/user_form_bloc.dart
+++ lib/src/users_managments/blocs/user_form_bloc/user_form_bloc.dart
@@ -1,1 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:fizyo_app_frontend/src/users_managments/blocs/user_form_bloc/user_form_event.dart';
+import 'package:fizyo_app_frontend/src/users_managments/blocs/user_form_bloc/user_form_state.dart';
 
+class UserFormBloc extends Bloc<UserFormEvent, UserFormState> {
+  // final AccountsService _accountsService;
+  // UserFormBloc(this._accountsService)
+  UserFormBloc() : super(const UserFormStateInitial(1 / 8, 1, 8)) {
+    on<UserFormEventSubmit>((event, emit) async {
+      emit(
+          UserFormStateSubmitting(event.currentStep / 8, event.currentStep, 8));
+      try {
+        // final user = User.fromJson(event.formData);
+        // await _accountsService.createAccount(user, null);
+        emit(UserFormStateSuccess(event.currentStep / 8, event.currentStep, 8));
+      } catch (e) {
+        emit(UserFormStateFailure(
+            e.toString(), event.currentStep / 8, event.currentStep, 8));
+      }
+    });
+  }
+}

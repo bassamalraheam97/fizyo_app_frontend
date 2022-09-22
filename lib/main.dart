ca792a46cc2447d9758617823f8e4e95fe68616f
Makeen93 <Makeen93@hotmail.com>
2022-09-22 20:27:34 +0300

    SomeFixes

--- lib/main.dart
+++ lib/main.dart
@@ -32,25 +32,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return DynamicColorBuilder(
-      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-        ColorScheme lightScheme;
-        ColorScheme darkScheme;
+        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+      ColorScheme lightScheme;
+      ColorScheme darkScheme;
 
-        if (lightDynamic != null && darkDynamic != null) {
-          lightScheme = lightDynamic.harmonized();
-          lightCustomColors = lightCustomColors.harmonized(lightScheme);
+      if (lightDynamic != null && darkDynamic != null) {
+        lightScheme = lightDynamic.harmonized();
+        lightCustomColors = lightCustomColors.harmonized(lightScheme);
 
-          // Repeat for the dark color scheme.
-          darkScheme = darkDynamic.harmonized();
-          darkCustomColors = darkCustomColors.harmonized(darkScheme);
-        } else {
-          // Otherwise, use fallback schemes.
-          lightScheme = lightColorScheme;
-          darkScheme = darkColorScheme;
-        }
+        // Repeat for the dark color scheme.
+        darkScheme = darkDynamic.harmonized();
+        darkCustomColors = darkCustomColors.harmonized(darkScheme);
+      } else {
+        // Otherwise, use fallback schemes.
+        lightScheme = lightColorScheme;
+        darkScheme = darkColorScheme;
+      }
 
-        return MaterialApp(
+      return MaterialApp(
           theme: ThemeData(
             useMaterial3: true,
             colorScheme: lightScheme,
@@ -63,79 +64,17 @@ class MyApp extends StatelessWidget {
             extensions: [darkCustomColors],
             fontFamily: 'Recoleta',
           ),
-          // builder: (context, child) {
-          //   child = botToastBuilder(context, child);
-          //   return child;
-          // },
           navigatorObservers: [BotToastNavigatorObserver()],
-          home: const MyHomePage(
-            title: "Fizyo App",
-          ),
-        );
-      },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  // late Service_provider pro;
-  void _incrementCounter() async {
-//   var options = BaseOptions(
-//   baseUrl: 'http://localhost:8000',
-//   connectTimeout: 5000,
-//   receiveTimeout: 3000,
-// );
-//   Dio dio = Dio(options);
-//   serviceProviderRepository _provider=HttpserviceProviderRepository(provider: dio);
-//
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+            body: LayoutBuilder(builder: (context, constraint) {
+              if (constraint.maxWidth > 500) {
+                return DesktopScreen();
+              } else {
+                return mobileScreen();
+              }
+            }),
+          ));
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: LayoutBuilder(builder: (context, constraint) {
-        if (constraint.maxWidth > 500) {
-          return DesktopScreen();
-        } else {
-          return mobileScreen();
-        }
-      }),
-    );
   }
 }
